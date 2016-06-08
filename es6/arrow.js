19f75eb0003f8921f41824fa5c77f333f022eaf1
alinsalaru <alin.salaru@gmail.com>
2016-06-08 22:14:32 +0300

    played with arrow functions

---
+++ es6/arrow.js
@@ -0,0 +1,32 @@
+//working with arrow functions
+var adderes5 = {
+  // the problem here is that the this binds to the window
+  base : 1,
+  
+  add : function(a) {
+    var f = function(v) {
+    v= v + this.base;
+    return v;
+    }
+	return f(a);
+  }
+};
+
+console.log(adderes5.add(2));
+
+var adder = {
+  // here we get rid of 'object method'
+  base : 1,
+    
+  add : function(a) {
+    var f = v => v + this.base;
+    return f(a);
+  }
+}
+
+console.log(adder.add(2));
+
+
+// es6 arrows:
+//  - shorter syntax
+//  - lexical this
